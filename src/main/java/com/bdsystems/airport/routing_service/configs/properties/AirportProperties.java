bfb33c9f94b5ecc7baaf3d71366c7603346efe1d
Benicius <beniciusclaro@hotmail.com>
2024-08-18 21:56:26 +0100

    remove volumes from docker image

--- src/main/java/com/bdsystems/airport/routing_service/configs/properties/AirportProperties.java
+++ src/main/java/com/bdsystems/airport/routing_service/configs/properties/AirportProperties.java
@@ -1,11 +1,13 @@
 package com.bdsystems.airport.routing_service.configs.properties;
 
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "airport.terminal")
 @Data
@@ -13,11 +15,4 @@ public class AirportProperties {
 	private List<NationalTerminal> national;
 	private List<InternationalTerminal> international;
 
-	public List<NationalTerminal> getNational() {
-		return national;
-	}
-
-	public List<InternationalTerminal> getInternational() {
-		return international;
-	}
 }
